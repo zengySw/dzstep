1c7b39a268ff28a8b79dd88c5e32e9928322ff5c
ZengySw <o.borvenok@gmail.com>
2025-09-12 21:59:21 +0300

    1

--- html/dz2/script.js
+++ html/dz2/script.js
@@ -1,35 +1,74 @@
-let a = parseInt(prompt("enter num", 2));
-let countA = 0;
-while (a >= countA) {
-    document.writeln("#");
-    countA++;
+// let a = parseInt(prompt("enter num", 2));
+// let countA = 0;
+// while (a >= countA) {
+//     document.writeln("#");
+//     countA++;
+// }
+
+// let countB = 0;
+// let b = prompt("enter num", 2);
+// while (parseInt(b) >= countB) {
+//     document.writeln(countB);
+//     countB++;
+// }
+
+// let c = parseInt(prompt("enter Power", 2));
+// let c1 = 0;
+// c1 = parseInt(prompt("enter num", 2));
+// let countC = 1;
+// let equals = 0;
+// while (c > countC) {
+//     countC++;
+//     equals += c1 * c1;
+// }
+// console.log(equals);
+
+// let d = parseInt(prompt("enter num", 4));
+// let countD = 0;
+// let equalsA = 1;
+// while (d > countD)
+// {
+//     equalsA += equalsA * countD
+//     countD++;
+// }
+// console.log(equalsA);
+
+//-------------------------------------
+// let e = 0;
+// do{
+//     e = parseInt(prompt("enter num", 6))    
+// } while(e !== 6);
+
+// let f = 1000;
+// let countF = 0;
+// do {
+//     countF++;
+//     f = f / 2;
+// } while(f > 50);
+// console.log(countF);
+
+
+// let g = parseInt(prompt("enter num", 2));
+// let countG = 0;
+// for (let count = 0; 100 >= count; count++) {
+//     if (g % count == 0) {countG++; console.log(count)}
+//     // console.log(count);
+// }
+// console.log(countG)
+
+let h1 = parseInt(prompt("enter min num", 0));
+let h2 = parseInt(prompt("enter max num", 12));
+let countH = 0;
+for (let count = h1; count <= h2; count++) {
+    if (countH == 0) {
+        countH = 0;
+        console.log(count);
+    }
+    else {
+        countH++;
+    }
+    console.log(`test ${count}`)
 }
 
-let countB = 0;
-let b = prompt("enter num", 2);
-while (parseInt(b) >= countB) {
-    document.writeln(countB);
-    countB++;
-}
-
-let c = parseInt(prompt("enter Power", 2));
-let c1 = 0;
-c1 = parseInt(prompt("enter num", 2));
-let countC = 1;
-let equals = 0;
-while (c > countC) {
-    countC++;
-    equals += c1 * c1;
-}
-console.log(equals);
-
-let d = parseInt(prompt("enter num", 4));
-let countD = 0;
-let equalsA = 1;
-while (d > countD)
-{
-    equalsA += equalsA * countD
-    countD++;
-}
-console.log(equalsA);
-
+let i = parseInt(prompt(""))
+for (let count = 0; count < )
